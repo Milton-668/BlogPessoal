5cee7d74d5f876507bc9bdfa8b75c03521cc7fe1
Milton-668 <miltonmjcp@gmail.com>
2021-03-02 09:54:18 -0300

    Otimizando o BlogPessaol #1

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -0,0 +1,5 @@
+package org.generation.blogPessoal.security;
+
+public class BasicSecurityConfig {
+
+}
--- blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package org.generation.blogPessoal.seguranca;
-
-public class BasicSecurityConfig {
-
-}
